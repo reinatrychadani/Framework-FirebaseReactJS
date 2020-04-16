0f82a9ae09e3bbc9a775b30073b1f5e5c83e097f
reinatrychadani <trycha67@gmail.com>
2020-04-16 12:45:41 +0700

    menambahkan file firebase.config.js

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyBRt5lkymIUr2p3eXBdizKOVL62GbzUibs",
+    authDomain: "frameworkfirebasereactjs.firebaseapp.com",
+    databaseURL: "https://frameworkfirebasereactjs.firebaseio.com",
+    projectId: "frameworkfirebasereactjs",
+    storageBucket: "frameworkfirebasereactjs.appspot.com",
+    messagingSenderId: "369243251226",
+    appId: "1:369243251226:web:c5defa201994eafd8627a3",
+    measurementId: "G-D59X6FHR51"
+}
